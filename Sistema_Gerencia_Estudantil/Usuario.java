d9472e203c978e037659a148241293fb8d9982e0
DavizinDoMaul <84157587+DavizinDoMaul@users.noreply.github.com>
2023-11-06 16:34:40 -0300

    Usuario.java

---
+++ Sistema_Gerencia_Estudantil/Usuario.java
@@ -0,0 +1,46 @@
+public class Usuario {
+    private String nome;
+    private String CPF;
+    private String senha;
+    private String ID;
+
+    public Usuario(String nome, String CPF, String senha, String ID) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.senha = senha;
+        this.ID = ID;
+    }
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
