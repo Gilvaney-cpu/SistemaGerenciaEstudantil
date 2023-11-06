74428b1be430b945cfd8ad94e06b05f32cfde0f7
DavizinDoMaul <84157587+DavizinDoMaul@users.noreply.github.com>
2023-11-06 16:48:07 -0300

    Update do diretor

--- Sistema_Gerencia_Estudantil/Diretor.java
+++ Sistema_Gerencia_Estudantil/Diretor.java
@@ -1,13 +1,13 @@
-ublic class Diretor extends Usuario {
+ublic class Diretor extends Usuario { //Atributos
     private int id;
     private String cargo;
-
+//instanciamento de atributos:
     public Diretor(int id, String cargo, String nome, String email, String senha) {
         super(nome, email, senha, "");
         this.id = id;
         this.cargo = cargo;
     }
-
+//get e set
     public int getId() {
         return id;
     }
