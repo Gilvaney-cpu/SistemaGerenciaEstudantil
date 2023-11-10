859c4caf43596365a86ba6735d8dc2383d50d75e
Gilvaney Leandro <GILVANEY.LEANDRO@UFRPE.BR>
2023-11-09 22:59:01 -0300

    Criação de metódos

--- Sistema_Gerencia_Estudantil/situacaoAluno.java
+++ Sistema_Gerencia_Estudantil/situacaoAluno.java
@@ -6,5 +6,41 @@ public enum situacaoAluno {
     RECUPERACAO,
     FINAL;
 
-    //Metódos para fazer
+    //Metódos
+
+    // Método para obter uma descrição textual da situação do aluno
+    public String getDescricao() {
+        switch (this) {
+            case APROVADO:
+                return "Aprovado";
+            case REPROVADO:
+                return "Reprovado";
+            case RECUPERACAO:
+                return "Recuperação";
+            case FINAL:
+                return "Final";
+            default:
+                return "Desconhecido";
+        }
+    }
+
+    // Método para verificar se a situação do aluno indica sucesso
+    public boolean isSuccess() {
+        return this == APROVADO;
+    }
+
+    // Método para verificar se a situação do aluno indica a necessidade de recuperação
+    public boolean needsRecuperacao() {
+        return this == RECUPERACAO;
+    }
+    
+    // Método para verificar se a situação do aluno indica reprovação
+    public boolean isReprovado() {
+        return this == REPROVADO;
+    }
+
+    // Método para verificar se a situação do aluno indica final
+    public boolean isFinal() {
+        return this == FINAL;
+    }
 }
